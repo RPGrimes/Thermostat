188f0029a5bd5b80892371702836346a9ce37312
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-24 11:21:15 +0100

    temperature decreases by 1 when .down() method is called, test passing

--- thermostat.test.js
+++ thermostat.test.js
@@ -1,13 +1,21 @@
 const Thermostat = require('./thermostat.js');
 
 describe("thermostat", () => {
-  const thermostat = new Thermostat;
+
   it('starts with a default temperature of 20', () => {
+    const thermostat = new Thermostat;
     expect(thermostat.getTemperature()).toBe(20);
   });
 
   it('increases the temperature by 1', () => {
+    const thermostat = new Thermostat;
     thermostat.up();
     expect(thermostat.getTemperature()).toBe(21)
   });
+
+  it('decreases the temperature by 1', () => {
+    const thermostat = new Thermostat;
+    thermostat.down();
+    expect(thermostat.getTemperature()).toBe(19)
+  })
 });
