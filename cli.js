6595f5ef9438051eeef98715737a0ec33ce0a414
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-25 10:16:19 +0100

    up and down methods can be called for thermostat in the command line

--- cli.js
+++ cli.js
@@ -1,2 +1,23 @@
 let readlineSync = require('readline-sync');
 const Thermostat = require('./thermostat.js');
+
+const thermostat = new Thermostat;
+
+let runThermostat = true;
+while(runThermostat === true) {
+  let currentTemperature = thermostat.getTemperature()
+  console.log(`Welcome to the thermostat control centre. The current temperature is ${currentTemperature}.`);
+  let command = readlineSync.question('Please enter a command > ')
+    if(command === 'up'){
+      thermostat.up();
+  } else if(command === 'down') {
+      thermostat.down();
+  }  else if (command === 'exit') {
+      runThermostat === false
+      console.log('You have exited the thermostat control centre.')
+      break
+  } else {
+    return 'Please enter a valid command.'
+  }
+}     
+
