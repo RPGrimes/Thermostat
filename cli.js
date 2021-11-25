a9bbd24fffbf91b63c05654a2120bb7f877170a3
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-25 10:30:52 +0100

    Added all thermostat.js methods to be accessible within cli.js

--- cli.js
+++ cli.js
@@ -4,15 +4,38 @@ const Thermostat = require('./thermostat.js');
 const thermostat = new Thermostat;
 
 let runThermostat = true;
+
+console.log('Welcome to the thermostat control centre.')
+
 while(runThermostat === true) {
   let currentTemperature = thermostat.getTemperature()
-  console.log(`Welcome to the thermostat control centre. The current temperature is ${currentTemperature}.`);
+  const logTemperature = `The current temperature is ${currentTemperature}.`
+  
+    if(currentTemperature === thermostat.minTemperature){
+      console.log(`${logTemperature} - minimum temperature reached`);
+  } else if(currentTemperature === thermostat.maxTemperature() && thermostat.powerSavingMode === true) {
+      console.log(`${logTemperature} - maximum temperature reached (psm on)`);
+  } else if(currentTemperature === thermostat.maxTemperature() && thermostat.powerSavingMode !== true) {
+    console.log(`${logTemperature} - maximum temperature reached (psm off)`);
+  } else {
+  console.log(`${logTemperature}`)
+}
+  
   let command = readlineSync.question('Please enter a command > ')
     if(command === 'up'){
       thermostat.up();
   } else if(command === 'down') {
       thermostat.down();
-  }  else if (command === 'exit') {
+  } else if(command === 'psm off') {
+      thermostat.setPowerSavingMode(false)
+  } else if(command === 'psm on') {
+      thermostat.setPowerSavingMode(true);
+  } else if(command === 'reset') {
+      thermostat.reset();
+  } else if(command === 'usage') {
+      thermostat.usage();
+      console.log(`Current usage is: ${thermostat.usage()}`)
+  } else if (command === 'exit') {
       runThermostat === false
       console.log('You have exited the thermostat control centre.')
       break
